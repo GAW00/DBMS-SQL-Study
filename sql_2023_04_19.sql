dc2f955ee5603faaf2db27982409efc425812770
GAWOO <ghost0970@naver.com>
2023-04-19 18:04:16 +0900

    Add sql_2023_04_19

---
+++ sql_2023_04_19.sql
@@ -0,0 +1,8 @@
+--MEMBER2 테이블 생성
+CREATE TABLE MEMBER2(
+    ID VARCHAR2(20)   --문자형 컬럼 ID, 자리수 : 20자리까지
+  , PW VARCHAR2(20)   --문자형 컬럼 PW, 자리수 : 20자리까지
+  , NAME VARCHAR2(20) --문자형 컬럼 NAME, 자리수 : 20자리까지
+  , CLASS NUMBER(1)   --숫자형 컬럼 ID, 자리수 : 1자리까지
+  , TEL VARCHAR2(20)  --문자형 컬럼 TEL, 자리수 : 20자리까지
+);
