bbc8ed5425800cf446136718078d111c8b14c172
GAWOO <ghost0970@naver.com>
2023-04-26 18:04:11 +0900

    Chage sql_2023_04_21

--- sql_2023_04_21.sql
+++ sql_2023_04_21.sql
@@ -21,3 +21,19 @@ CREATE TABLE memberT(
 );
 
 DROP TABLE memberT;
+DROP TABLE BOARDT;
+
+select * from boardt;
+DELETE FROM BOARDT;
+
+CREATE TABLE boardT(
+    B_ID NUMBER(5) primary key, -- 1. 글번호
+    B_NAME VARCHAR2(20),        -- 2. 글쓴이
+    B_EMAIL VARCHAR2(50),       -- 3. 글쓴이 메일 주소
+    B_TITLE VARCHAR2(80),       -- 4. 글 제목
+    B_CONTENT VARCHAR2(3000)    -- 5. 글 내용
+);
+
+INSERT INTO BOARDT(B_ID, B_NAME, B_EMAIL, B_TITLE, B_CONTENT) VALUES((SELECT NVL(MAX(B_ID), 0) + 1 FROM BOARDT), 'A', 'A', 'A', 'A');
+
+COMMIT;
