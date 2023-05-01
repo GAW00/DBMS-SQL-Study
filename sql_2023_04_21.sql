9f30e5d8e31b5fc12a66537e59010d420d83df04
GAWOO <ghost0970@naver.com>
2023-05-01 18:27:27 +0900

    Change sql_2023_05_01

--- sql_2023_04_21.sql
+++ sql_2023_04_21.sql
@@ -24,16 +24,19 @@ DROP TABLE memberT;
 DROP TABLE BOARDT;
 
 select * from boardt;
-DELETE FROM BOARDT;
+select rownum, a.* from(select * from boardt) a;
+DELETE FROM BOARDT where b_id=1;
 
 CREATE TABLE boardT(
     B_ID NUMBER(5) primary key, -- 1. 글번호
     B_NAME VARCHAR2(20),        -- 2. 글쓴이
     B_EMAIL VARCHAR2(50),       -- 3. 글쓴이 메일 주소
     B_TITLE VARCHAR2(80),       -- 4. 글 제목
-    B_CONTENT VARCHAR2(3000)    -- 5. 글 내용
+    B_CONTENT VARCHAR2(3000),   -- 5. 글 내용
+    B_DATE DATE,                -- 6. 작성일자
+    B_HIT number(5) default 0,  -- 7. 조회수
+    B_PWD varchar2(12)          -- 8. 비밀번호
 );
 
-INSERT INTO BOARDT(B_ID, B_NAME, B_EMAIL, B_TITLE, B_CONTENT) VALUES((SELECT NVL(MAX(B_ID), 0) + 1 FROM BOARDT), 'A', 'A', 'A', 'A');
 
 COMMIT;
